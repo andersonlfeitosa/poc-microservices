3b069ea310f7e124384b593bdc1c2ea90f12d438
Anderson Lobo Feitosa <andersonlf@gmail.com>
2018-02-08 16:05:51 -0200

    novas configuracoes no eureka server

--- microservice1/src/main/java/com/andersonlfeitosa/openshift/Application.java
+++ microservice1/src/main/java/com/andersonlfeitosa/openshift/Application.java
@@ -2,10 +2,11 @@ package com.andersonlfeitosa.openshift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-//@EnableHystrix
+@EnableDiscoveryClient
+// @EnableHystrix
 public class Application {
 
   public static void main(String[] args) {
